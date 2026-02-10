06c0d49e566235839b2391896a4db5d17c2e560e
Donald Burr <dburr@vctlabs.com>
2026-02-10 11:27:33 -0800

    Add camera overrides from SB3d repo

--- bin/setup.sh
+++ bin/setup.sh
@@ -211,6 +211,14 @@ setup_target_files() {
         TARGET_DIR=$BSP_DIR/Linux_for_Tegra/rootfs/home/$TARGET_USER
         sudo mkdir -p $TARGET_DIR
 
+        # copy in camera configuration
+        OVERRIDE_TARGET_DIR=$BSP_DIR/Linux_for_Tegra/var/nvidia/nvcam/settings
+        sudo rm -v $OVERRIDE_TARGET_DIR/nvcam_cache_*
+        sudo rm -v $OVERRIDE_TARGET_DIR/serial_no_*
+        sudo cp -v $WORKING_DIR/target/camera_overrides/camera_overrides.isp $OVERRIDE_TARGET_DIR
+        sudo chmod 664 $OVERRIDE_TARGET_DIR/camera_overrides.isp
+        sudo chown root:root $OVERRIDE_TARGET_DIR/camera_overrides.isp
+
         # copy in setup/test scripts
         sudo cp $WORKING_DIR/target/* $TARGET_DIR
         sudo chmod +x $TARGET_DIR/*.sh
---
+++ target/camera_overrides/configTX2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# clone this directory grom github.
+
+# open a terminal here. and execote this script
+
+./jetsonSdkInstall32.7.1.sh
+./SB3d-camera-calibration-files/jetsonSdkInstall32.7.1.sh
+./installJsonServer.sh
+./installSb3dDev.sh
+
+#echo open QtCreator and compile the camera app and ui
+
+pause
+
+./install.sh
+
